6aee56d2e2ae92c2300befecf602d6b7ef80ecfd
Emirhan Kırman <emirhankirman616@hotmail.com>
2026-04-24 13:03:31 +0300

    Add Category CRUD operations

---
+++ ECommerceProject.Web/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+using ECommerceProject.Data.Context;
+using ECommerceProject.Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceProject.Web.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public CategoryController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // LIST
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _context.Categories.ToListAsync();
+        return View(categories);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Category model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        model.CreatedDate = DateTime.UtcNow;
+
+        _context.Categories.Add(model);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // EDIT - GET
+    public async Task<IActionResult> Edit(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+            return NotFound();
+
+        return View(category);
+    }
+
+    // EDIT - POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, Category model)
+    {
+        if (id != model.Id)
+            return NotFound();
+
+        if (!ModelState.IsValid)
+            return View(model);
+
+        var entity = await _context.Categories.FindAsync(id);
+        if (entity == null)
+            return NotFound();
+
+        entity.Name = model.Name;
+        entity.Description = model.Description;
+        entity.IsActive = model.IsActive;
+        entity.UpdatedDate = DateTime.UtcNow;
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    // DELETE - GET
+    public async Task<IActionResult> Delete(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+            return NotFound();
+
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+            return NotFound();
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
